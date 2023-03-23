b896c59dedaa66a6ab937970d1e1b6a8373e633b
vahid <vahidrajabi.pro7913@gmail.com>
2023-03-23 13:22:02 +0430

    create authenticated user

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -147,3 +146,7 @@ LOGIN_REDIRECT_URL = 'home'
     
 # Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# static file config
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[str(BASE_DIR.joinpath('static'))]
--- config/urls.py
+++ config/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 # from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('pages.urls')),
+    path('', include('pages.urls')),
     # path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('accounts/',include('accounts.urls')),
-    path("book/",include('books.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path("book/", include('books.urls')),
 
 ]
